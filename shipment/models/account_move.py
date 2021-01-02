2271b8cafb720a2d6164041de02fb5998b6d965c
oscar <a.adam@exp-sa.com>
2021-01-02 11:01:03 +0200

    code documentation

--- shipment/models/account_move.py
+++ shipment/models/account_move.py
@@ -4,7 +4,9 @@ from odoo.exceptions import ValidationError, UserError
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
-    
+    #add policy ref to account move ,
+    #when create bill or invoice ,
+    # it will automatically link to its policy by this field
     policy_id = fields.Many2one(
         string='Policy',
         comodel_name='shipment.order',
