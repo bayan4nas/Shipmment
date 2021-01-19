9d600060a2606bc595081e706c281ef1d2b425e0
oscar <a.adam@exp-sa.com>
2021-01-19 21:06:20 +0200

    add compute currency amount and some fields for reprot purpose

--- shipment/models/account_move.py
+++ shipment/models/account_move.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError, UserError
+from odoo.tools.misc import formatLang, format_date, get_lang
 
 
 class AccountMove(models.Model):
@@ -12,11 +13,20 @@ class AccountMove(models.Model):
         comodel_name='shipment.order',
     )
     
-    charge = fields.Char(
-        string='Charge',
+    rate_currency_id = fields.Many2one(
+        string='Currency For Layout',
+        comodel_name='res.currency', 
+        default=lambda self: self.env.user.company_id.currency_id
     )
+
+    
+    charge_id = fields.Many2one(
+        string='Charge',
+        comodel_name='payment.charge.type', 
+    )
+
     charge_amount = fields.Float(
-        string='Charge Amount',
+        string='Charge Rate',
     )
 
     company_currency_id = fields.Many2one(
@@ -26,17 +36,49 @@ class AccountMove(models.Model):
         
     )
     
-    def compute_currency(self,total,date_order,currency_id ,is_company):
+   
+    
+    def compute_currency_with_rate(self,mve,total,rate,currency_id ):
+        amount = rate * total
+        print("am,ount=========",amount)
+        lang_env = mve.with_context(lang=mve.partner_id.lang).env
+        # amount_convert = formatLang(lang_env, amount, currency_obj=currency_id)
+        print("amount conver============",amount)
+        return amount
        #call the method from report with amount , currency and date to get amount in company currency and vis versa
-        company_currency = self.env.user.company_id.currency_id
-        currency_id = self.env['res.currency'].search([('id', '=', currency_id)])
-        # if company_currency.id == currency_id.id:
-        if is_company:
-            amount_convert = company_currency._convert(total, currency_id, self.env.user.company_id, date_order or fields.Date.today())
+        # company_currency = self.env.user.company_id.currency_id
+        # currency_id = self.env['res.currency'].search([('id', '=', currency_id)])
+        # # if company_currency.id == currency_id.id:
+        # print(":currency=======",currency_id.name)
+        # print(":company_currency=======",company_currency.name)
+        # amount_convert = currency_id._convert(total, company_currency, self.env.user.company_id, date_order or fields.Date.today())
+        
+        # lang_env = mve.with_context(lang=mve.partner_id.lang).env
+        # amount_convert = formatLang(lang_env, amount_convert, currency_obj=currency_id)
+        # return amount_convert
                 # amount_convert = currency_id.with_context(date=date_order).compute(total,company_currency )
                 #     return round(amount_convert,3)
                 # amount_convert = currency_id.with_context(date=date_order).compute(total,company_currency)
-            return round(amount_convert,3)
         # amount_convert = currency_id._convert(total, company_currency, self.env.user.company_id, date_order or fields.Date.today())
+        # return total
+
+    def compute_currency_without_rate(self,total,date_order,currency_id ):
+        
+       #call the method from report with amount , currency and date to get amount in company currency and vis versa
+        company_currency = self.env.user.company_id.currency_id
+        # currency_id = self.env['res.currency'].search([('id', '=', currency_id)])
+        # # if company_currency.id == currency_id.id:
+        # print(":currency=======",currency_id.name)
+        # print(":company_currency=======",company_currency.name)
+        if currency_id != company_currency:
+            amount_convert = currency_id._convert(total, company_currency, self.env.user.company_id, date_order or fields.Date.today())
+        # lang_env = mve.with_context(lang=mve.partner_id.lang).env
+        # amount_convert = formatLang(lang_env, amount_convert, currency_obj=currency_id)
+            return round(amount_convert,3)
         return total
+                # amount_convert = currency_id.with_context(date=date_order).compute(total,company_currency )
+                #     return round(amount_convert,3)
+                # amount_convert = currency_id.with_context(date=date_order).compute(total,company_currency)
+        # amount_convert = currency_id._convert(total, company_currency, self.env.user.company_id, date_order or fields.Date.today())
+        # return total
        
